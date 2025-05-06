0b806a26f1e531717c26f65d2e67c79cf63a8ac8
Daud Abdiwahab <daudwahab1320@gmail.com>
2025-05-06 15:03:17 +0300

    inital commit

---
+++ datatypes.py
@@ -0,0 +1,23 @@
+"""
+Rules associated with variables .
+1. naming convention is snake_case
+2.cannot use keywords i.e. def, class, if, else, elif, for, while, try, except, finally
+3.variables must contain values when initialized
+4.we use indentation to define scope/block.
+5.have descriptive names.
+6.functions must be defined with def keyword
+7. we do not use semicolon at the end of the line
+
+"""
+#strings
+message="intro to python"
+print(type(message))
+
+#interpolation in python using f-string
+# f-string is a way to format strings in python
+first_name="John"
+full_name=f"{first_name} Doe"
+print(full_name)
+
+
+
