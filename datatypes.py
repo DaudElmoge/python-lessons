6beb5c880838f8bd3a4fdf992366d03ad9177cbb
Daud Abdiwahab <daudwahab1320@gmail.com>
2025-05-06 17:06:24 +0300

    booleans,lists,tuples

--- datatypes.py
+++ datatypes.py
@@ -35,3 +35,46 @@ print(type(7/3))# float
 print(type(6/3))# float
 
 print(round(2.5+1.4)) #4-rounded off from 3.9 to 4
+
+
+#booleans
+"""
+booleans are logical statements that can be either True or False
+"""
+is_adult=True #True should be capitalized
+is_child=False #False should be capitalized
+print(bool(""))#False-> empty string is False
+print(bool(0))#False-> 0 is False
+print(bool(1))#True-> 1 is True
+print(bool("False"))#True-> non-empty string is True
+
+#dictionaries->they are basically objects
+
+person={
+    "name":"John",
+    "age":4
+}
+print(person["age"])#4
+print(person.get("age"))#4
+print(person["name"])#John
+print(person.get("name"))#John
+
+person={
+    "name":"John",
+    "age":4,
+    "address":{"county":"Mombasa"
+               }
+}
+print(person["address"]["county"])#Mombasa
+print(person.get("address").get("county"))#Mombasa
+
+#lists,tuples
+#tuples are immutable 
+#lists are mutable
+
+scores=(1,2,3,4,5) #example of tuple
+cart=["milk","bread","eggs"] #example of list
+
+#none -> combines null and undefined
+day=None #example of none, used to indicate that a variable has no value
+day="Monday" #assigned value after value is known
