4d48a069b56c13f0770f43bca5f4300552bfb13d
Daud Abdiwahab <daudwahab1320@gmail.com>
2025-05-08 15:02:03 +0300

    added comments

--- coderbyte.js
+++ coderbyte.js
@@ -1,6 +1,10 @@
 function camelCase(str) {
-    return str.replace(/\W+(.)/g, function (_,chr) {
-        return chr.toUpperCase()
-    });
+  return str.replace(/\W+(.)/g, function (_, chr) {
+    // The regular expression /\W+(.)/g looks for one or more non-word characters (\W+)
+    // followed by any character (.), and captures that character.
+    return chr.toUpperCase();
+    // For each match, this function returns the captured character (chr)
+    // converted to uppercase
+  });
 }
-console.log(camelCase("Bob loves coding"))
+console.log(camelCase("Bob loves coding"));
